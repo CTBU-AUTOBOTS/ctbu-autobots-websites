7fadb47fbe1acf0fae9a712a895b3e03ae81df64
Lwrzgo <1312899391@qq.com>
2026-06-21 00:28:10 +0800

    post(home): 更新桌面横幅图片资源

--- apps/home/src/config.ts
+++ apps/home/src/config.ts
@@ -119,6 +119,7 @@ export const siteConfig: SiteConfig = {
 		// 支持单张图片或图片数组，当数组长度 > 1 时自动启用轮播
 		src: {
 			desktop: [
+				"/assets/desktop-banner/d00.webp",
 				"/assets/desktop-banner/d01.webp",
 				"/assets/desktop-banner/d02.webp",
 				"/assets/desktop-banner/d03.webp",
